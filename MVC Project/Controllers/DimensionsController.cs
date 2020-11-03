f3d4b0bfd41951587786e058ade8add4dce4e8c3
ThatoMphuthi <38042647+ThatoMphuthi@users.noreply.github.com>
2020-11-03 22:48:56 +0200

    Add project files.

---
+++ MVC Project/Controllers/DimensionsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC_Project.Data;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class DimensionsController : Controller
+    {
+        private readonly DimensionDBContext _context;
+
+        public DimensionsController(DimensionDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dimensions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dimension.ToListAsync());
+        }
+
+        // GET: Dimensions/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dimension = await _context.Dimension
+                .FirstOrDefaultAsync(m => m.EmployeeNumber == id);
+            if (dimension == null)
+            {
+                return NotFound();
+            }
+
+            return View(dimension);
+        }
+
+        // GET: Dimensions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dimensions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Age,Attrition,BusinessTravel,DailyRate,Department,DistanceFromHome,Education,EducationField,EmployeeCount,EmployeeNumber,EnvironmentSatisfaction,Gender,HourlyRate,JobInvolvement,JobLevel,JobRole,JobSatisfaction,MaritalStatus,MonthlyIncome,MonthlyRate,NumCompaniesWorked,Over18,OverTime,PercentSalaryHike,PerformanceRating,RelationshipSatisfaction,StandardHours,StockOptionLevel,TotalWorkingYears,TrainingTimesLastYear,WorkLifeBalance,YearsAtCompany,YearsInCurrentRole,YearsSinceLastPromotion,YearsWithCurrManager")] Dimension dimension)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dimension);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dimension);
+        }
+
+        // GET: Dimensions/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dimension = await _context.Dimension.FindAsync(id);
+            if (dimension == null)
+            {
+                return NotFound();
+            }
+            return View(dimension);
+        }
+
+        // POST: Dimensions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Age,Attrition,BusinessTravel,DailyRate,Department,DistanceFromHome,Education,EducationField,EmployeeCount,EmployeeNumber,EnvironmentSatisfaction,Gender,HourlyRate,JobInvolvement,JobLevel,JobRole,JobSatisfaction,MaritalStatus,MonthlyIncome,MonthlyRate,NumCompaniesWorked,Over18,OverTime,PercentSalaryHike,PerformanceRating,RelationshipSatisfaction,StandardHours,StockOptionLevel,TotalWorkingYears,TrainingTimesLastYear,WorkLifeBalance,YearsAtCompany,YearsInCurrentRole,YearsSinceLastPromotion,YearsWithCurrManager")] Dimension dimension)
+        {
+            if (id != dimension.EmployeeNumber)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dimension);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DimensionExists(dimension.EmployeeNumber))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dimension);
+        }
+
+        // GET: Dimensions/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dimension = await _context.Dimension
+                .FirstOrDefaultAsync(m => m.EmployeeNumber == id);
+            if (dimension == null)
+            {
+                return NotFound();
+            }
+
+            return View(dimension);
+        }
+
+        // POST: Dimensions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var dimension = await _context.Dimension.FindAsync(id);
+            _context.Dimension.Remove(dimension);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DimensionExists(string id)
+        {
+            return _context.Dimension.Any(e => e.EmployeeNumber == id);
+        }
+    }
+}
---
+++ MVC Project/Data/DimensionDBContext.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using MVC_Project.Models;
+
+namespace MVC_Project.Data
+{
+    public partial class DimensionDBContext : DbContext
+    {
+        public DimensionDBContext()
+        {
+        }
+
+        public DimensionDBContext(DbContextOptions<DimensionDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AspNetRoleClaims> AspNetRoleClaims { get; set; }
+        public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
+        public virtual DbSet<AspNetUserClaims> AspNetUserClaims { get; set; }
+        public virtual DbSet<AspNetUserLogins> AspNetUserLogins { get; set; }
+        public virtual DbSet<AspNetUserRoles> AspNetUserRoles { get; set; }
+        public virtual DbSet<AspNetUserTokens> AspNetUserTokens { get; set; }
+        public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
+        public virtual DbSet<Dimension> Dimension { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#pragma warning disable
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Initial Catalog=DimensionDB;Data Source=THATO\\MSSQLSERVER01;Trusted_Connection=true");
+            }
+#pragma warning disable
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AspNetRoleClaims>(entity =>
+            {
+                entity.HasIndex(e => e.RoleId);
+
+                entity.Property(e => e.RoleId).IsRequired();
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.AspNetRoleClaims)
+                    .HasForeignKey(d => d.RoleId);
+            });
+
+            modelBuilder.Entity<AspNetRoles>(entity =>
+            {
+                entity.HasIndex(e => e.NormalizedName)
+                    .HasName("RoleNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedName] IS NOT NULL)");
+
+                entity.Property(e => e.Name).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedName).HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<AspNetUserClaims>(entity =>
+            {
+                entity.HasIndex(e => e.UserId);
+
+                entity.Property(e => e.UserId).IsRequired();
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserClaims)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUserLogins>(entity =>
+            {
+                entity.HasKey(e => new { e.LoginProvider, e.ProviderKey });
+
+                entity.HasIndex(e => e.UserId);
+
+                entity.Property(e => e.LoginProvider).HasMaxLength(128);
+
+                entity.Property(e => e.ProviderKey).HasMaxLength(128);
+
+                entity.Property(e => e.UserId).IsRequired();
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserLogins)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUserRoles>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.RoleId });
+
+                entity.HasIndex(e => e.RoleId);
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.AspNetUserRoles)
+                    .HasForeignKey(d => d.RoleId);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserRoles)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUserTokens>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.LoginProvider, e.Name });
+
+                entity.Property(e => e.LoginProvider).HasMaxLength(128);
+
+                entity.Property(e => e.Name).HasMaxLength(128);
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.AspNetUserTokens)
+                    .HasForeignKey(d => d.UserId);
+            });
+
+            modelBuilder.Entity<AspNetUsers>(entity =>
+            {
+                entity.HasIndex(e => e.NormalizedEmail)
+                    .HasName("EmailIndex");
+
+                entity.HasIndex(e => e.NormalizedUserName)
+                    .HasName("UserNameIndex")
+                    .IsUnique()
+                    .HasFilter("([NormalizedUserName] IS NOT NULL)");
+
+                entity.Property(e => e.Email).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedEmail).HasMaxLength(256);
+
+                entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+
+                entity.Property(e => e.UserName).HasMaxLength(256);
+            });
+
+            modelBuilder.Entity<Dimension>(entity =>
+            {
+                entity.HasKey(x=>x.EmployeeNumber);
+
+                entity.Property(e => e.Attrition)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.BusinessTravel)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Department)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.DistanceFromHome)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Education)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.EducationField)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.EmployeeCount)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.EmployeeNumber)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.EnvironmentSatisfaction)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.JobInvolvement)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.JobLevel)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.JobRole)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.JobSatisfaction)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.MaritalStatus)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.NumCompaniesWorked)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Over18)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.OverTime)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.RelationshipSatisfaction)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.StockOptionLevel)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.TotalWorkingYears)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.TrainingTimesLastYear)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.WorkLifeBalance)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.YearsAtCompany)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.YearsInCurrentRole)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.YearsSinceLastPromotion)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.YearsWithCurrManager)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
